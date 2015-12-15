2a95c4f126e4ff1ea7e8168d73cd33a133cf51a3
Maximilian Weber <mw2852@columbia.edu>
2015-12-15 18:56:33 -0500

    routing to playlistMaker

---
+++ playlistMaker.js
@@ -0,0 +1,10 @@
+var echo = require('echonest');
+var express = require('express');
+var router = express.Router();
+
+var createPLaylist = function(query){
+	
+}
+
+router.createPLaylist = createPLaylist;
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var playlistMaker = require('../playlistMaker');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/search', function(req, res, next) {
+	playlistMaker.createPlaylist(req.query);
 });
 
 module.exports = router;
