55cb76bc9cbbb37ead75fcc73b86ac012ebd8a6a
Naina Prasad <prasad.naina@gmail.com>
2015-12-15 20:36:06 -0500

    JS/HTML to populate widget with tracks

---
+++ fillWidget.js
@@ -0,0 +1,13 @@
+var widgetInternalHTML = '<iframe src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'
+
+
+for (var i = 0; i < spotifyIdArray.length; i++){
+	widgetInternalHTML += spotifyIdArray[i] += ",";
+}
+
+widgetInternalHTML += '" frameborder="0" allowtransparency="true"></iframe>';
+
+
+
+
+$("#divForWidget").html(widgetInternalHTML)
