7cbe6320f0db20e9897d472b5d6243da311e7d42
Naina Prasad <prasad.naina@gmail.com>
2015-12-16 14:36:30 -0500

    fillWidget function for Miles

--- fillWidget.js
+++ fillWidget.js
@@ -1,13 +1,13 @@
-var widgetInternalHTML = '<iframe src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'
+var populateWidget = function(spotifyIdArray){
+	var widgetInternalHTML = '<iframe src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'
 
+	for (var i = 0; i < spotifyIdArray.length; i++){
+		widgetInternalHTML += spotifyIdArray[i] += ",";
+	}
 
-for (var i = 0; i < spotifyIdArray.length; i++){
-	widgetInternalHTML += spotifyIdArray[i] += ",";
+	widgetInternalHTML += '" frameborder="0" allowtransparency="true"></iframe>';
+
+	$("#divForWidget").html(widgetInternalHTML);
 }
 
-widgetInternalHTML += '" frameborder="0" allowtransparency="true"></iframe>';
 
-
-
-
-$("#divForWidget").html(widgetInternalHTML)
